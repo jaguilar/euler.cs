bfacbe0d0e4ad9ef143d4a52c4501d6ff92a3ac7
James Aguilar <aguilar.james@gmail.com>
2014-10-12 14:45:12 -0700

    tests for prime gen

--- euler/UtilTest.cs
+++ euler/UtilTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace euler
 {
@@ -21,6 +22,18 @@ namespace euler
 			Assert.That(Util.Digit(123, 1), Is.EqualTo(2));
 			Assert.That(Util.Digit(123, 2), Is.EqualTo(1));
 			Assert.That(Util.Digit(123, 3), Is.EqualTo(0));
+		}
+
+		[Test()]
+		public void TestPrimes() {
+			var ps = Util.Primes().Take(100);
+			Assert.That(ps, Has.Some.EqualTo(2));
+			Assert.That(ps, Has.Some.EqualTo(3));
+			Assert.That(ps, Has.Some.EqualTo(7));
+			Assert.That(ps, Has.Some.EqualTo(71));
+			Assert.That(ps, Has.None.EqualTo(10));
+			Assert.That(ps, Has.None.EqualTo(4));
+			Assert.That(ps, Has.None.EqualTo(99));
 		}
 	}
 }
